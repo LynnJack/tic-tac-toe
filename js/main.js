fa1d43f9d65f547a70f865af73bdc7bea9a67cc1
Lorin Thwaits <lorin@Lorins-MacBook-Air.local>
2014-10-14 18:12:39 -0700

    Ya we added a crazy directive with stars

--- js/main.js
+++ js/main.js
@@ -1,7 +1,47 @@
-var tictactoeApp = angular.module('tictactoeApp', ["firebase"]);
-tictactoeApp.controller('tttController', ["$scope", "$firebase", function ($scope, $firebase) {
+var myScope;
+
+var app = angular.module('TictactoeApp', ["firebase"]);
+
+app.directive('myDirective', function () {
+  return {
+  	template: '<ul class="rating">' +
+                  '<li x-ng-repeat="star in stars" class="filled" x-ng-click="toggle($index)">' +
+                      '\u2605' +
+                  '</li>' +
+                '</ul>',
+    restrict: 'A',
+    scope: {
+    	ratingValue: "="	// Two-way data bind this thing!
+    },
+    link: function (scope, elem, attrs) {
+      console.log("Directive", scope, elem, attrs);
+
+      scope.$watch("ratingValue", function(newThing, oldThing){
+  			scope.stars = [];
+				for(var x = 0; x < parseInt(newThing); x++)
+				{
+					scope.stars.push({});
+				}
+      });
+
+      scope.toggle = function(index) {
+		    scope.ratingValue = index + 1;
+			};
+    }
+  }
+});
+
+app.controller('tttController', ["$scope", "$http", "$firebase", function ($scope, $http, $firebase) {
+	myScope = $scope;
+	$scope.someVar = 5;
+
+	// var promise = $http.get("https://api.github.com/repos/lorint/AndrewIG/issues");
+	// promise.success(function(data){
+	// 	$scope.issues = data;
+	// });
+
 	// this is creating new Firebase connection using the Firebase object
-	var ref = new Firebase("https://turnip.firebaseio.com/dude");
+	var ref = new Firebase("https://turnip.firebaseio.com");
 	//pass the firebase connection/object to angularfire
 	var sync = $firebase(ref); // enables the firebase binding
 	var firebase = sync.$asObject();
